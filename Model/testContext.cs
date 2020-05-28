1cf60b81b739ce6b0e1e6d3db9c84fef562f7ec9
Ranjith Tadisina <58748826+ranjith-tadisina@users.noreply.github.com>
2020-05-27 20:42:39 -0500

    Update testContext.cs

--- Model/testContext.cs
+++ Model/testContext.cs
@@ -26,7 +26,7 @@ namespace SampleDBConn.Model
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Server=localhost:1433;User Id=SA;Password=Omsairam@99;Database=test;Trusted_Connection=False;");
+                optionsBuilder.UseSqlServer(@"Server=localhost;User Id=sa;Password=Omsairam@99;Database=test;Trusted_Connection=False;");
 
             }
         }
